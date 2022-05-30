bbd0d0fcbcf77e5f709d0f53cb8ebb4fbbfc2744
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-05-30 15:35:09 +0100

    changed the database credentials to live heroku postgres db

--- Core/settings.py
+++ Core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [config("HOST"), "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [config("WEB_HOST"), "localhost", "127.0.0.1"]
 
 
 # Application definition
